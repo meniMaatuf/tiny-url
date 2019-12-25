1e9806253b4e1373e19ff291f6dfa4ac11940ffd
meniMaatuf <meni.m770@gmail.com>
2019-12-25 16:35:24 +0200

    upload

--- app.js
+++ app.js
@@ -23,19 +23,19 @@ app.use('/', indexRouter);
 app.use('/api/', urlsRouter);
 
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header(
-      "Access-Control-Allow-Headers",
-      "Content-type,Accept,x-access-token,X-Key"
-  );
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-  } else {
-    next();
-  }
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+//   res.header(
+//       "Access-Control-Allow-Headers",
+//       "Content-type,Accept,x-access-token,X-Key"
+//   );
+//   if (req.method === "OPTIONS") {
+//     res.status(200).end();
+//   } else {
+//     next();
+//   }
+// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,47 @@
 var express = require('express');
 const shortid = require("shortid");
+const validUrl = require("valid-url");
 var router = express.Router();
 
 let shortUrls = [];
 
 /* GET users listing. */
-router.get('/api/item/:shortcode', function(req, res, next) {
-  const code = req.params.code;
-  const item = shortUrls.find(i => i.urlCode === code);
-  return res.status(200).json(item || {});
+router.get('/api/item/:shortcode', function (req, res, next) {
+    const code = req.params.code;
+    const item = shortUrls.find(i => i.urlCode === code);
+    return res.status(200).json(item || {});
 
 });
 
-router.post('/api/item', function(req, res, next) {
-  const {originalUrl} = req.body;
-  const urlCode = shortid.generate();
-  const item = {
-    originalUrl,
-    shortUrl: `http://localhost/api/item/${urlCode}`,
-    urlCode
-  };
+router.post('/api/item', function (req, res, next) {
+    const {originalUrl} = req.body;
+    const urlCode = shortid.generate();
 
-  shortUrls.push(item);
+    if (validUrl.isUri(originalUrl)) {
 
-  res.status(200).json(item);
-});
+        const item = {
+            originalUrl,
+            shortUrl: `http://localhost/api/item/${urlCode}`,
+            urlCode
+        };
 
-router.get('/:shortcode', function(req, res, next) {
-  const code = req.params.code;
-  const item = shortUrls.find(i => i.urlCode === code);
-  return res.redirect(item.originalUrl);
+        shortUrls.push(item);
+
+        res.status(200).json(item);
+    } else {
+        return res
+            .status(401)
+            .json(
+                "Invalid Url"
+            );
+    }
+})
+;
+
+router.get('/:shortcode', function (req, res, next) {
+    const code = req.params.code;
+    const item = shortUrls.find(i => i.urlCode === code);
+    return res.redirect(item.originalUrl);
 
 });
 
